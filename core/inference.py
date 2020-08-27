add 1 more option for heatmap generation, and fix one bug in ROI center calculation

core/inference.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from utils.transforms import transform_preds
 
-
+global_img_idx = 0
 def get_max_preds(batch_heatmaps):
     '''
     get predictions from score maps
@@ -49,6 +49,7 @@ def get_heatmap_center_preds(batch_heatmaps):
     get predictions from score maps
     heatmaps: numpy.ndarray([batch_size, 1, height, width])
     '''
+    global global_img_idx
     assert isinstance(batch_heatmaps, np.ndarray), \
         'batch_heatmaps should be numpy.ndarray'
     assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
@@ -72,6 +73,8 @@ def get_heatmap_center_preds(batch_heatmaps):
         img = np.uint8( batch_heatmaps[idx, :, :, :] * 255)
         img = cv2.medianBlur(img, 5)
         cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        # param1：使用HOUGH_GRADIENT方法检测圆形时，传递给Canny边缘检测器的两个阈值的较大值。
+        # param2：使用HOUGH_GRADIENT方法检测圆形时，检测圆形的累加器阈值，阈值越大检测的圆形越精确
         # https://blog.csdn.net/weixin_42904405/article/details/82814768
         # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghcircles/py_houghcircles.html
         circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
@@ -79,6 +82,7 @@ def get_heatmap_center_preds(batch_heatmaps):
         max_rad = 0.0
         # import pdb
         # pdb.set_trace()
+        global_img_idx += 1
         if circles is not None:
             for i in circles[0, :]:
                 # get the maximum outer circle
@@ -88,11 +92,12 @@ def get_heatmap_center_preds(batch_heatmaps):
                     preds[idx, 0, 0] = i[0]
                     preds[idx, 0, 1] = i[1]
 
-            #     cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            #     cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-            #
-            # tmp_filename = "test_roi_heatmap_%d.png" %(idx)
-            # cv2.imwrite(tmp_filename, cimg)
+                # cv2.circle(cimg, (i[0], i[1]), int(i[2]), (0, 255, 0), 2)
+                # cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+                # print("MV heatmap %d: center: (%d, %d), current_rad: %.02f" %(global_img_idx%400, i[0], i[1], current_rad))
+            # roi_heatmap_file = "test_roi_heatmap_%d_%d_%d.png" %(global_img_idx%400, i[0], i[1])
+            # cv2.imwrite(roi_heatmap_file, cimg)
 
     return preds
 

dataset/dataset.py
@@ -57,18 +57,6 @@ common_aug_func = iaa.Sequential(
     iaa.Grayscale(alpha=0.5)
 ])
 
-def create_circular_mask(h, w, center=None, radius=None):
-    if center is None: # use the middle of the image
-        center = [int(w/2), int(h/2)]
-    if radius is None: # use the smallest distance between the center and image walls
-        radius = min(center[0], center[1], w-center[0], h-center[1])
-
-    Y, X = np.ogrid[:h, :w]
-    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
-
-    mask = dist_from_center <= radius
-    return mask
-
 
 class FoveaDataset(Dataset):
     def __init__(self, cfg, root, image_set, is_train, transform=None):
@@ -95,12 +83,30 @@ class FoveaDataset(Dataset):
         self.region_radius = cfg.MODEL.REGION_RADIUS
         self.clahe_enaled = cfg.TRAIN.DATA_CLAHE
         self.mv_idea = cfg.TRAIN.MV_IDEA
+        self.mv_idea_hm1 = cfg.TRAIN.MV_IDEA_HM1
 
         self.transform = transform
         self.db = []
 
     def evaluate(self, preds, output_dir):
         raise NotImplementedError
+
+    def create_circular_mask(self, h, w, center=None, radius=None):
+        if center is None:  # use the middle of the image
+            center = [int(w / 2), int(h / 2)]
+        if radius is None:  # use the smallest distance between the center and image walls
+            radius = min(center[0], center[1], w - center[0], h - center[1])
+
+        Y, X = np.ogrid[:h, :w]
+        dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
+
+        if self.mv_idea_hm1:
+            # xiaofeng changed it -- use a continuous gray level change circle, center = 0
+            dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2) / radius
+            return dist_from_center
+        else:
+            mask = dist_from_center <= radius
+            return mask
 
     def _get_db(self,):
         raise NotImplementedError
@@ -395,10 +401,22 @@ class FoveaDataset(Dataset):
 
             # Re-apply the target generation process
             image_size = np.array([region_size, region_size], np.int32)
-            heatmap_roi = np.zeros((1,
-                                   region_size,
-                                   region_size),
-                                   dtype=np.float32)
+            if self.mv_idea:
+                if self.mv_idea_hm1:
+                    heatmap_roi = np.ones((1,
+                                            region_size,
+                                            region_size),
+                                           dtype=np.float32)
+                else:
+                    heatmap_roi = np.zeros((1,
+                                            region_size,
+                                            region_size),
+                                           dtype=np.float32)
+            else:
+                heatmap_roi = np.zeros((1,
+                                       region_size,
+                                       region_size),
+                                       dtype=np.float32)
             target_weight = np.array([1.], np.float32)
 
             # TODO: xiaofeng comment: it should bigger = self.sigma x feat_stride ??
@@ -439,10 +457,16 @@ class FoveaDataset(Dataset):
             # https://stackoverflow.com/questions/44865023/how-can-i-create-a-circular-mask-for-a-numpy-array
             if self.mv_idea:
                 # create a circle
-                g = np.ones((size, size), np.uint8)
-                mask = create_circular_mask(size, size)
-                g[mask] = 1
-                g[~mask] = 0
+                if self.mv_idea_hm1:
+                    # method 2 -- set the center point 0 -- black
+                    g = self.create_circular_mask(size, size)
+                else:
+                # set the center point 0
+                    g = np.ones((size, size), np.uint8)
+                    mask = self.create_circular_mask(size, size)
+                    g[mask] = 1
+                    g[~mask] = 0
+
             else:
                 g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * (self.sigma_roi) ** 2))
 

models/fovea_net.py
@@ -13,7 +13,7 @@ import cv2
 import copy
 import numpy as np
 
-from core.inference import get_max_preds
+from core.inference import get_max_preds, get_heatmap_center_preds
 from utils.transforms import crop_and_resize
 from efficientnet.model import EfficientNet
 
@@ -545,6 +545,7 @@ class FoveaNet(nn.Module):
         #                        bias=False)
         # self.bn3 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
         # self.relu = nn.ReLU(inplace=True)
+        self.orig_stemnet = False
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False)
         self.conv1_1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2, bias=False)
         self.bn1 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
@@ -1001,12 +1002,18 @@ class FoveaNet(nn.Module):
                     data_numpy = data_numpy.cuda().permute([0, 3, 1, 2])
 
                     if self.cfg.TRAIN.MV_IDEA:
-                        roi_feats_hr = self.relu(self.bn1(self.conv1_1(data_numpy)))
+                        if self.orig_stemnet:
+                            roi_feats_hr = self.relu(self.bn1(self.conv1(data_numpy)))
+                        else:
+                            roi_feats_hr = self.relu(self.bn1(self.conv1_1(data_numpy)))
                     else:
                         roi_feats_hr = self.relu(self.bn1(self.conv1(data_numpy)))
                 else:
                     if self.cfg.TRAIN.MV_IDEA:
-                        roi_feats_hr = self.relu(self.bn1(self.conv1_1(input_roi)))
+                        if self.orig_stemnet:
+                            roi_feats_hr = self.relu(self.bn1(self.conv1(input_roi)))
+                        else:
+                            roi_feats_hr = self.relu(self.bn1(self.conv1_1(input_roi)))
                     else:
                         roi_feats_hr = self.relu(self.bn1(self.conv1(input_roi)))     # strip = 2
 
@@ -1014,7 +1021,8 @@ class FoveaNet(nn.Module):
                 roi_feats_hr = self.relu(self.bn2(self.conv2(roi_feats_hr)))  # (batch, 64, 128, 128)
                 # xiaofeng add for k=3 layer for ROI
                 if self.cfg.TRAIN.MV_IDEA:
-                    roi_feats_hr = self.relu(self.bn3(self.conv3(roi_feats_hr)))  # (batch, 64, 128, 128)
+                    if not self.orig_stemnet:
+                        roi_feats_hr = self.relu(self.bn3(self.conv3(roi_feats_hr)))  # (batch, 64, 128, 128)
 
                 roi_feats_hr = self.subpixel_up_by2(roi_feats_hr)             # (batch, 16, 256, 256)
 
@@ -1029,7 +1037,11 @@ class FoveaNet(nn.Module):
 
             if infer_roi:
                 # Get initial location from heatmap
-                loc_pred_init = get_max_preds(heatmap_roi_pred.cpu().numpy())[0][:, 0, :]
+                if self.cfg.TRAIN.MV_IDEA:
+                    loc_pred_init = get_heatmap_center_preds(heatmap_roi_pred.cpu().numpy())[:, 0, :]
+                else:
+                    loc_pred_init = get_max_preds(heatmap_roi_pred.cpu().numpy())[0][:, 0, :]
+
                 loc_pred_init = torch.FloatTensor(loc_pred_init).cuda(non_blocking=True)
                 meta.update({'pixel_in_roi': loc_pred_init.cpu()})
             else:

utils/vis.py
@@ -9,7 +9,7 @@ import torchvision
 import torch.nn.functional as F
 import cv2
 
-from core.inference import get_max_preds
+from core.inference import get_max_preds, get_heatmap_center_preds
 
 
 def save_batch_image_with_fovea(batch_image, batch_fovea, file_name, nrow=8, padding=2, ds_factor=1):
@@ -52,7 +52,7 @@ def save_batch_image_with_fovea(batch_image, batch_fovea, file_name, nrow=8, pad
 
 
 def save_batch_heatmaps(batch_image, batch_heatmaps, file_name,
-                        normalize=True):
+                        normalize=True, MV_IDEA=False):
     # visualize only the first sample
     batch_image = batch_image[:1, :, :, :]
     batch_heatmaps = batch_heatmaps[:1, :]
@@ -74,7 +74,10 @@ def save_batch_heatmaps(batch_image, batch_heatmaps, file_name,
                            3),
                           dtype=np.uint8)
 
-    preds, maxvals = get_max_preds(batch_heatmaps.detach().cpu().numpy())
+    if MV_IDEA:
+        preds = get_heatmap_center_preds(batch_heatmaps.detach().cpu().numpy())
+    else:
+        preds, maxvals = get_max_preds(batch_heatmaps.detach().cpu().numpy())
 
     for i in range(batch_size):
         image = batch_image[i].mul(255)\
@@ -140,13 +143,17 @@ def save_debug_images(config,
 
     # HRNET has not ROI heatmap and region
     if not config.TRAIN.HRNET_ONLY:
+        if config.TRAIN.MV_IDEA:
+            MV_Center = True
+        else:
+            MV_Center = False
         if config.DEBUG.SAVE_HEATMAP_ROI_GT and heatmap_roi is not None:
             save_batch_heatmaps(
-                input_roi, heatmap_roi, '{}_hm_roi_gt.jpg'.format(prefix)
+                input_roi, heatmap_roi, '{}_hm_roi_gt.jpg'.format(prefix), MV_IDEA=MV_Center
             )
         if config.DEBUG.SAVE_HEATMAP_ROI_PRED:
             save_batch_heatmaps(
-                input_roi, heatmap_roi_pred, '{}_hm_roi_pred.jpg'.format(prefix)
+                input_roi, heatmap_roi_pred, '{}_hm_roi_pred.jpg'.format(prefix), MV_IDEA=MV_Center
             )
         if config.DEBUG.SAVE_FOVEA_ROI_GT and heatmap_roi is not None:
             save_batch_image_with_fovea(